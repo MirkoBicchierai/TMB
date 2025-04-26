some fix and add parameters to masking sequences

mainRL.py
@@ -258,6 +258,7 @@ def generate(model, train_dataloader, iteration, c, device, infos, text_model, s
     model.train()
 
     dataset = {
+
         "r": [],
         "xt_1": [],
         "xt": [],
@@ -285,7 +286,10 @@ def generate(model, train_dataloader, iteration, c, device, infos, text_model, s
         batch = {k: v.to(device) if isinstance(v, torch.Tensor) else v for k, v in batch.items()}
 
         tx_emb, tx_emb_uncond = get_embeddings_2(text_model, batch, c.num_gen_per_prompt, device)
-        infos["all_lengths"] = batch["length"].repeat(c.num_gen_per_prompt)
+
+        if not c.sequence_fixed:
+            infos["all_lengths"] = batch["length"].repeat(c.num_gen_per_prompt)
+
         sequences, results_by_timestep = model.diffusionRL(tx_emb=tx_emb, tx_emb_uncond=tx_emb_uncond, infos=infos)
 
         reward, tmr = tmr_reward_special(sequences, infos, smplh, batch["tmr_text"].repeat(c.num_gen_per_prompt, 1),
@@ -554,7 +558,10 @@ def test(model, dataloader, device, infos, text_model, smplh, joints_renderer, s
         batch = {k: v.to(device) if isinstance(v, torch.Tensor) else v for k, v in batch.items()}
         with torch.no_grad():
             tx_emb, tx_emb_uncond = get_embeddings(text_model, batch, device)
-            infos["all_lengths"] = batch["length"]
+
+            if not c.sequence_fixed:
+                infos["all_lengths"] = batch["length"]
+
             sequences, _ = model.diffusionRL(tx_emb, tx_emb_uncond, infos)
 
             if (ty_log == "Validation" and batch_idx == 0) or ty_log == "Test":
@@ -706,16 +713,18 @@ def main(c: DictConfig):
     test_dataset = instantiate(cfg.data, split=str(c.dataset_name) + "test")
 
     infos = {
-        #"all_lengths": torch.tensor(np.full(2048, int(c.time * c.fps))).to(device),
         "featsname": cfg.motion_features,
         "fps": c.fps,
         "guidance_weight": c.guidance_weight
     }
 
+    if c.sequence_fixed:
+        infos["all_lengths"] = torch.tensor(np.full(2048, int(c.time * c.fps))).to(device)
+
     train_dataloader = DataLoader(
         train_dataset,
         batch_size=c.num_prompts_dataset,
-        shuffle=True,  # True
+        shuffle=True,
         drop_last=False,
         num_workers=c.num_workers,
         collate_fn=train_dataset.collate_fn

src/model/gaussian.py
@@ -28,6 +28,13 @@ def masked(tensor, mask):
     tensor[~mask] = 0.0
     return tensor
 
+def nan_masked(tensor, mask):
+    if isinstance(tensor, list):
+        return [masked(t, mask) for t in tensor]
+    tensor[~mask] =  float('nan')
+    return tensor
+
+
 
 logger = logging.getLogger(__name__)
 
@@ -256,18 +263,18 @@ class GaussianDiffusion(DiffuserBase):
 
     def p_sample_2(self, xt, y, t, guidance_weight):
         # guided forward
-        output_cond = self.denoiser(xt, y, t)
+        output_cond = masked(self.denoiser(xt, y, t), y["mask"])
 
         if guidance_weight == 1.0:
             output = output_cond
         else:
             y_uncond = y.copy()
             y_uncond["tx"] = y_uncond["tx_uncond"]
-            output_uncond = self.denoiser(xt, y_uncond, t)
+            output_uncond = masked(self.denoiser(xt, y_uncond, t), y["mask"])
             output = output_uncond + guidance_weight * (output_cond - output_uncond)
 
         mean, sigma = self.q_posterior_distribution_from_output_and_xt(output, xt, t)
-
+        mean = masked(mean, y["mask"])
         # empiricamnte va bene fino a 0.3 poi si rompe la generazione
         sigma = torch.max(sigma, torch.tensor(0.1))
 
@@ -277,7 +284,7 @@ class GaussianDiffusion(DiffuserBase):
         return x_out, xstart, mean, sigma
 
 
-    def diffusionRL(self, tx_emb=None, tx_emb_uncond=None, infos=None, guidance_weight=1.0, y=None, t=None, xt=None,A=None):
+    def diffusionRL(self,tx_emb=None, tx_emb_uncond=None, infos=None, guidance_weight=1.0, y=None, t=None, xt=None,A=None):
 
         device = self.device
 
@@ -299,6 +306,7 @@ class GaussianDiffusion(DiffuserBase):
             nfeats = self.denoiser.nfeats
 
             shape = bs, duration, nfeats
+
             xt = masked(torch.randn(shape, device=device), mask)
 
             # iterator = range(self.timesteps - 1, -1, -1)
@@ -317,7 +325,8 @@ class GaussianDiffusion(DiffuserBase):
                 xt = masked(xt, mask)
 
                 log_likelihood = self.log_likelihood(xt, mean, sigma)
-                log_prob = log_likelihood.mean(dim=[1, 2])
+                log_likelihood = nan_masked(log_likelihood, mask)
+                log_prob = log_likelihood.nanmean(dim=[1, 2])
 
                 results[diffusion_step] = {
 
@@ -349,6 +358,7 @@ class GaussianDiffusion(DiffuserBase):
             xt_pred, _, mean, sigma = self.p_sample_2(xt, y, t, infos["guidance_weight"])
             xt_pred = masked(xt_pred, y["mask"])
             log_likelihood = self.log_likelihood(A, mean, sigma)
-            log_probs = log_likelihood.mean(dim=[1, 2])
+            log_likelihood = nan_masked(log_likelihood, y["mask"])
+            log_probs = log_likelihood.nanmean(dim=[1, 2])
 
             return log_probs, xt_pred